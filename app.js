50bdc1452f1fa02910520f6d0c27e144c1b81b42
yoonseungjae <tmdwo5811@gmail.com>
2020-10-20 23:49:57 +0900

    express 서버 세팅  포트

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 'use strict';
 
 const express = require('express');
+const app = express();
+
+
+const port = 2500;
+app.listen(port, () => {
+    console.log(`sDesign NODE SERVER - RUNNING ON ${port}`);
+})
