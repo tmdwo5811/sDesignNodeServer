6c8f8dcd925a94c35b44b984e38026d80ffebc38
tmdwo5811 <tmdwo5811@gmail.com>
2020-11-28 19:11:32 +0900

    :zap: 프리티어 적용

--- repositories/account.repository.js
+++ repositories/account.repository.js
@@ -1,33 +1,33 @@
-'use strict'
+"use strict";
 
-const accountModel = require('../models/account.model');
+const accountModel = require("../models/account.model");
 
 exports.create = async (query) => {
-    try {
-        const result = await accountModel.create([query]);
-        return result;
-    } catch (e) {
-        console.log(e);
-        throw e;
-    }
-}
+  try {
+    const result = await accountModel.create([query]);
+    return result;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
 
 exports.updateOne = async (filter, doc, options) => {
-    try {
-        const result = await accountModel.updateOne(filter, doc, options);
-        return result;
-    } catch (e) {
-        console.log(e);
-        throw e;
-    }
-}
+  try {
+    const result = await accountModel.updateOne(filter, doc, options);
+    return result;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
 
 exports.findOne = async (query, projection, options) => {
-    try {
-        const result = await accountModel.findOne(query, projection).lean(options.lean);
-        return result;
-    } catch (e) {
-        console.log(e);
-        throw e;
-    }
-}
+  try {
+    const result = await accountModel.findOne(query, projection).lean(options.lean);
+    return result;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
--- repositories/file.repository.js
+++ repositories/file.repository.js
@@ -1,40 +1,41 @@
-'use strict'
+"use strict";
 
-const fileModel = require('../models/file.model');
+const fileModel = require("../models/file.model");
 
+// prettier 테스트
 exports.create = async (query) => {
-    try {
-        const result = await fileModel.create([query]);
-        return result;
-    } catch (e) {
-        console.log(e);
-        throw e;
-    }
-}
+  try {
+    const result = await fileModel.create([query]);
+    return result;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
 
 exports.paginate = async (query, options, next, previous) => {
-    try {
-        const result = await fileModel.paginate({
-            query,
-            next,
-            previous,
-            limit: options.limit
-        })
-        return result;
-    } catch (e) {
-        console.log(e);
-        throw e;
-    }
-}
+  try {
+    const result = await fileModel.paginate({
+      query,
+      next,
+      previous,
+      limit: options.limit,
+    });
+    return result;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
 
 exports.findAll = async (query, projection, populate, options) => {
-    return fileModel.find(query, projection).populate(populate).sort(options.sort);
-}
+  return fileModel.find(query, projection).populate(populate).sort(options.sort);
+};
 
 exports.findOne = async (query, projection) => {
-    return fileModel.findOne(query, projection);
-}
+  return fileModel.findOne(query, projection);
+};
 
 exports.removeOne = async (query) => {
-    return fileModel.deleteOne(query);
-}
+  return fileModel.deleteOne(query);
+};
