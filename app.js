82dca8f4608fcf6a3e852413f3c2e8fbf222f222
tmdwo5811 <tmdwo5811@gmail.com>
2020-11-28 20:12:53 +0900

    :memo: Todo 작성

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const mongoOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
+// https 적용 해야함
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
   console.log("Connected to sDesign mongod Server.");
