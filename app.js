5f20bc32d21de887fef55c6a3f03b5019fe88e3e
tmdwo5811 <tmdwo5811@gmail.com>
2020-11-28 19:15:12 +0900

    :zap: 프리티어 route.js 포맷 규칙 수정

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const http = require('http');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const http = require("http");
 const app = express();
 const db = mongoose.connection;
 
-
 app.use(cors());
 app.use(bodyParser.json());
-app.set('views', __dirname + '/utils');
-app.use(express.static('files'));
-app.engine('html', require('ejs').renderFile);
-require('./routes')(app);
-const docs = require('./routes/swagger.route');
+app.set("views", __dirname + "/utils");
+app.use(express.static("files"));
+app.engine("html", require("ejs").renderFile);
+require("./routes")(app);
+const docs = require("./routes/swagger.route");
 const mongoUrl = `mongodb+srv://nodeapi:nC7ZznzhonIg15hd@sDesign.8etgi.mongodb.net/sdesign?retryWrites=true&w=majority`;
 const mongoOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-    console.log('Connected to sDesign mongod Server.');
-})
-mongoose.connect(
-    mongoUrl,mongoOptions
-);
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to sDesign mongod Server.");
+});
+mongoose.connect(mongoUrl, mongoOptions);
 
 const server = http.createServer(app);
 const port = 2500;
 server.listen(port, () => {
-    console.log(`sDesign NODE SERVER - RUNNING ON ${port}`);
-})
+  console.log(`sDesign NODE SERVER - RUNNING ON ${port}`);
+});
--- routes/account.route.js
+++ routes/account.route.js
@@ -1,21 +1,25 @@
-'use strict';
+"use strict";
 
-const accountController = require('../controllers/account.controller');
-const tokenController = require('../controllers/token.controller');
+const accountController = require("../controllers/account.controller");
+const tokenController = require("../controllers/token.controller");
 module.exports = (router) => {
-    router.route("/create/account")
+  router
+    .route("/create/account")
     .post(accountController.createAccount);
 
-    router.route("/confirm/account")
+  router
+    .route("/confirm/account")
     .get(accountController.confirmAccount);
 
-    router.route("/login")
+  router
+    .route("/login")
     .post(accountController.accountLogin);
 
-    router.route("/token/test")
+  router
+    .route("/token/test")
     .get(
-        tokenController.verifyToken,
-        accountController.tokenTest
-        )
-    return router;
-}
+      tokenController.verifyToken,
+      accountController.tokenTest
+    );
+  return router;
+};
